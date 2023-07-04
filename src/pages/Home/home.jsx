e207be4a53c545545c96374c4f6f02332b89cdf4
Kaustubh Manglurkar <46368989+KaustubhM1997@users.noreply.github.com>
2023-07-05 01:09:25 +0530

    added: database

---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,17 @@
+import "./home.css"
+export default function Home() {
+  return (
+    <>
+      {/* <h1>This is the Home page</h1> */}
+
+      <nav className="navbar">
+        
+          <p className="nav-header">SocialNetwork</p>
+          {/* <button>Login</button> */}
+
+          <i class="fa-solid fa-right-from-bracket"></i>
+        
+      </nav>
+    </>
+  );
+}
